7312d8ab0f44d18e8f841ce60498bad23400a71f
andresfegome <andresfegome15@gmail.com>
2022-11-20 14:28:24 -0500

    folders

---
+++ src/pages/Proyectos.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import "../style/proyectos.css";
+
+const style = document.documentElement.style;
+const Proyectos = () => {
+  let mode = useSelector(state => state.thema);
+
+  useEffect(() => {
+    if (mode === "true") {
+      style.setProperty("--colorFondoProyecto", "#0b0d17d5");
+      style.setProperty("--colorTextoProyecto", "white");
+      style.setProperty("--colorPurpureProyecto", "#2EC5CE");
+    } else if (mode === "false") {
+      style.setProperty("--colorFondoProyecto", "rgba(156, 148, 148, 0.486)");
+      style.setProperty("--colorTextoProyecto", "black");
+      style.setProperty("--colorPurpureProyecto", "#8c30f5c4");
+    }
+  }, [mode]);
+
+  return (
+    <div className='container-proyectos'>
+      <div className='card-proyecto'>
+        <div className='card img-1'></div>
+        <p>
+          <span>Ecommerce</span>
+          <a href='/#/home'>Ver</a>
+        </p>
+      </div>
+      <div className='card-proyecto'>
+        <div className='card img-2'></div>
+        <p>
+          <span>Pokedex</span>
+          <a href='/#/home'>Ver</a>
+        </p>
+      </div>
+      <div className='card-proyecto'>
+        <div className='card img-3'></div>
+        <p>
+          <span>Portafolio vanilla</span>
+          <a href='/#/home'>Ver</a>
+        </p>
+      </div>
+      <div className='card-proyecto'>
+        <div className='card img-4'></div>
+        <p>
+          <span>ClimApp</span>
+          <a href='/#/home'>Ver</a>
+        </p>
+      </div>
+      <div className='card-proyecto'>
+        <div className='card img-5'></div>
+        <p>
+          <span>Citas</span>
+          <a href='/#/home'>Ver</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Proyectos;
